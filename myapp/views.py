16ec9b9989c468231bc9f48b39c202ec85fc6e32
Roy <royassisg@gmail.com>
2021-05-28 13:41:13 +0300

    Add api request with access token

--- myapp/views.py
+++ myapp/views.py
@@ -1,8 +1,8 @@
+import requests
 from django.http import HttpResponse
 from django.shortcuts import render
 import json
-
-import pandas as pd
+from django.conf import settings
 from facebook_business.adobjects.adaccount import AdAccount
 from facebook_business.api import FacebookAdsApi
 
@@ -17,4 +17,12 @@ def get_token(request):
         body_unicode = request.body.decode('utf-8')
         body = json.loads(body_unicode)
         print(body)
+
+        my_access_token = body.get("accessToken")
+        params = {'fields': "name",
+                  'access_token': my_access_token}
+
+        res_insights = requests.get("https://graph.facebook.com/v10.0/me?", params)
+        print(res_insights.status_code)
+
         return HttpResponse("Succ")
