c1f8a435564d923c2d943708e20d40a2b047a941
Roy <royassisg@gmail.com>
2021-05-28 13:15:11 +0300

    Add requirements.txt

--- myapp/views.py
+++ myapp/views.py
@@ -2,6 +2,11 @@ from django.http import HttpResponse
 from django.shortcuts import render
 import json
 
+import pandas as pd
+from facebook_business.adobjects.adaccount import AdAccount
+from facebook_business.api import FacebookAdsApi
+
+
 def index(request):
     return render(request, 'myapp/index.html')
 
